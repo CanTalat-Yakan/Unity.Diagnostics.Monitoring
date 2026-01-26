9e478cf86672fc600a8603341335c0cc05c3f6b1
CanTalat-Yakan <can.talat.yakan@gmail.com>
2026-01-26 20:41:19 +0100

    ⠀⠀⠀⠀⠀

--- Runtime/RuntimeMonitor.cs
+++ Runtime/RuntimeMonitor.cs
@@ -4,32 +4,21 @@ using UnityEngine;
 
 namespace UnityEssentials
 {
-    /// <summary>
-    /// Zero-setup auto registrar for the ImGui Runtime Monitor.
-    /// 
-    /// This is intentionally NOT dependency injection.
-    /// It only discovers MonoBehaviours that contain [Monitor] members and registers them for display.
-    /// </summary>
     internal static class RuntimeMonitor
     {
-        private static readonly BindingFlags SBindingFlags =
-            BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic;
+        private static readonly BindingFlags s_bindingFlags = RuntimeDiscovery.AllMembers;
 
         [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterSceneLoad)]
         private static void OnAfterSceneLoad()
         {
             try
             {
-                var monoBehaviours = FindMonoBehaviours();
+                var monoBehaviours = RuntimeDiscovery.FindAllMonoBehaviours();
                 for (var i = 0; i < monoBehaviours.Length; i++)
                 {
                     var mb = monoBehaviours[i];
-                    if (mb == null)
-                        continue;
-
-                    if (!HasAnyMonitorMembers(mb))
-                        continue;
-
+                    if (mb == null) continue;
+                    if (!HasAnyMonitorMembers(mb)) continue;
                     RuntimeMonitorImGui.Register(mb);
                 }
             }
@@ -39,67 +28,38 @@ namespace UnityEssentials
             }
         }
 
-        private static MonoBehaviour[] FindMonoBehaviours()
-        {
-#if UNITY_2023_1_OR_NEWER
-            // Include inactive as well. DI and monitoring both benefit from predictable discovery.
-            return UnityEngine.Object.FindObjectsByType<MonoBehaviour>(FindObjectsSortMode.InstanceID);
-#else
-            return UnityEngine.Object.FindObjectsOfType<MonoBehaviour>(includeInactive: true);
-#endif
-        }
-
         private static bool HasAnyMonitorMembers(MonoBehaviour obj)
         {
             var type = obj.GetType();
 
-            // Walk inheritance chain so private members in base types are found too.
-            for (var t = type; t != null; t = t.BaseType)
+            return RuntimeDiscovery.AnyMemberInHierarchy(type, member =>
             {
-                // Fields
-                var fields = t.GetFields(SBindingFlags);
-                for (var i = 0; i < fields.Length; i++)
-                {
-                    var field = fields[i];
-                    if (field.IsDefined(typeof(System.Runtime.CompilerServices.CompilerGeneratedAttribute), true))
-                        continue;
+                if (RuntimeDiscovery.IsCompilerGenerated(member))
+                    return false;
 
-                    if (Attribute.IsDefined(field, typeof(MonitorAttribute), inherit: true))
-                        return true;
-                }
+                // Fields
+                if (member is FieldInfo field)
+                    return RuntimeDiscovery.HasAttribute(field, typeof(MonitorAttribute), inherit: true);
 
                 // Properties
-                var props = t.GetProperties(SBindingFlags);
-                for (var i = 0; i < props.Length; i++)
+                if (member is PropertyInfo prop)
                 {
-                    var prop = props[i];
-                    if (prop.GetIndexParameters().Length != 0)
-                        continue;
-
-                    if (Attribute.IsDefined(prop, typeof(MonitorAttribute), inherit: true))
-                        return true;
+                    if (prop.GetIndexParameters().Length != 0) return false;
+                    return RuntimeDiscovery.HasAttribute(prop, typeof(MonitorAttribute), inherit: true);
                 }
 
                 // Methods
-                var methods = t.GetMethods(SBindingFlags);
-                for (var i = 0; i < methods.Length; i++)
+                if (member is MethodInfo method)
                 {
-                    var method = methods[i];
-                    if (method.IsGenericMethod)
-                        continue;
-
-                    if (method.GetParameters().Length != 0)
-                        continue;
-
-                    if (method.ReturnType == typeof(void))
-                        continue;
-
-                    if (Attribute.IsDefined(method, typeof(MonitorAttribute), inherit: true))
-                        return true;
+                    if (method.IsGenericMethod) return false;
+                    if (method.GetParameters().Length != 0) return false;
+                    if (method.ReturnType == typeof(void)) return false;
+                    return RuntimeDiscovery.HasAttribute(method, typeof(MonitorAttribute), inherit: true);
                 }
-            }
 
-            return false;
+                return false;
+
+            }, s_bindingFlags);
         }
     }
 }
